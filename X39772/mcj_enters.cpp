524051d9e972d85dae61800c5c0714a860921c2b
jooanna4 <joana.bruguera@gmail.com>
2023-09-13 17:12:37 +0200

    ei

--- X39772/mcj_enters.cpp
+++ X39772/mcj_enters.cpp
@@ -8,18 +8,28 @@ mcj_enters::mcj_enters(const mcj_enters &cj) {
     for (int i = 0; i < size; i++) {
         v[i] = cj.v[i];
     }
+    size = cj.size;
 }
 
 mcj_enters::~mcj_enters() { }
 
 mcj_enters& mcj_enters::operator=(const mcj_enters &cj) {
-    mcj_enters aux(cj);
-    return aux;
+    size = cj.size;
+    for (int i = 0; i < size; i++) {
+        v[i] = cj.v[i];
+    }
+    return *this;
 }
 
 // Insereix l’enter e en el multiconjunt.
 void mcj_enters::insereix(int e) {
-    v[size] = e;
+    int aux;
+    for (int i = 0; i < size; i++) 
+        if (e <= v[i]) aux = i;
+    for (int i = size; aux < i; i--) {
+        v[i+1] = v[i];
+    }
+    v[aux] = e;
     size++;
 }
 
@@ -27,11 +37,29 @@ void mcj_enters::insereix(int e) {
 // el mètode, usant el segon multiconjunt com argument. P.e.: a.restar(b) fa que el nou valor d’a sigui
 // A - B, on A i B són els valors originals dels objectes a i b.
 void mcj_enters::unir(const mcj_enters& B) {
-
+    
 }
 
 void mcj_enters::intersectar(const mcj_enters& B) {
-
+    if (*this == B) return;
+    else {
+        int aux[size];
+        int i = 0, j = 0, mida, it = 0;
+        while ((i < size) or (j < B.size)) {
+            if (v[i] == B.v[j]) {
+                i++; j++;
+                aux[it] = v[i];
+                it++;
+            }
+            else if (v[i] < B.v[j]) i++
+            else j++;
+        }
+        size = it;
+        // Copia de la intersecció
+        for (int i = 0; i < size; i++) {
+            v[i] = aux[i];
+        }
+    }
 }
 
 void mcj_enters::restar(const mcj_enters& B) {
@@ -57,6 +85,7 @@ mcj_enters mcj_enters::operator-(const mcj_enters& B) const {
 bool mcj_enters::conte(int e) const {
     for (int i = 0; i < size; i++) {
         if (v[i] == e) return true;
+        if (v[i] > e) return false;
     }
     return false;
 }
@@ -64,16 +93,18 @@ bool mcj_enters::conte(int e) const {
 // Retornen els elements màxim i mínim del multiconjunt, respectivament.
 // El seu comportament no està definit si el multiconjunt és buit.
 int mcj_enters::max() const {
-
+    if (size == 0) return -1;
+    else return v[size-1];
 }
 
 int mcj_enters::min() const {
-
+    if (size == 0) return -1;
+    else return v[0];
 }
 
 // Retorna el nombre d’elements (la cardinalitat) del multiconjunt.
 int mcj_enters::card() const {
-
+    return size;
 }
 
 
@@ -82,11 +113,14 @@ int mcj_enters::card() const {
 // mateixos elements; != retorna cert si i només si els
 // multiconjunts són diferents.
 bool mcj_enters::operator==(const mcj_enters& B) const {
-
+    for (int i = 0; i < size; i++) {
+        if (v[i] != B.v[i]) return false;
+    }
+    return true;
 }
 
 bool mcj_enters::operator!=(const mcj_enters& B) const {
-
+    return !(this == B);
 }
 
 
@@ -94,5 +128,10 @@ bool mcj_enters::operator!=(const mcj_enters& B) const {
 // el canal de sortida os; el format és [e1 e2 ... en], és a dir, amb
 // espais entre els elements i tancant la seqüència amb corxets.
 void mcj_enters::print(ostream& os) const {
-
+    os << "[";
+    for (int i = 0; i < size; i++) {
+        os << v[i];
+        if (i!=size-1) os << " ";
+    }
+    os << "]";
 }
