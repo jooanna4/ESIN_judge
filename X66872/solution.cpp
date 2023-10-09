007a72bb264610fcb8017f4a70c5869c1636c0a3
jooanna4 <joana.bruguera@gmail.com>
2023-10-09 11:50:27 +0200

    fet

--- X66872/solution.cpp
+++ X66872/solution.cpp
@@ -3,15 +3,21 @@
 void Llista::separa(Llista &l2) {
     if (_long == 0 or _long == 1) return;
 
-    node *ant = _prim, *aux = _prim->seg, *segu = _prim->seg->seg, *pnou;
-    for (int i = 1; i <= _long; i++) {
-        // si es parell
-        if (i%2 == 0) {
-            
-        }
-        // si es imparell
-        else {
-
+    node *p(_prim), *p2ant(nullptr);
+    while (p != nullptr) {
+        node *pseg (p->seg);
+        if (pseg != nullptr) {
+            p->seg = pseg->seg;
+            if (p2ant == nullptr)
+                l2._prim = pseg;
+            else
+                p2ant->seg = pseg;
+            p = p->seg;
+            p2ant = pseg;
         }
     }
+    if (p2ant != nullptr)
+        p2ant->seg = nullptr;
+    l2._long = _long / 2;
+    _long = _long - l2._long;
 }
